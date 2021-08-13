63e9cc2eb0645e6b2f3c60a8e0596600ca933325
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-08-13 16:38:51 +0200

    Audio extensions corrected

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -44,15 +44,15 @@ const opponentPlayers = [
     skill: 3,
     src: "assets/imgs/opponents/lucy.jpg",
     greeting: "assets/audio/voices/american-grt-2.mp3",
-    gloating: "assets/audio/voices/american-gloat-1.mp3",
-    angerfail: "assets/audio/voices/american-anger-1.mp3",
+    gloating: "assets/audio/voices/american-gloat-1.wav",
+    angerfail: "assets/audio/voices/american-anger-1.wav",
   },
   {
     name: "Colonel Enrique Diaz Firpi III",
     skill: 2,
     src: "assets/imgs/opponents/colonel.jpg",
     greeting: "assets/audio/voices/enrique-grt-1.mp3",
-    gloating: "assets/audio/voices/enrique-gloat-1.mp3",
+    gloating: "assets/audio/voices/enrique-gloat-1new.mp3",
     angerfail: "assets/audio/voices/enrique-anger-3.mp3",
   },
   {
